df33cbf34dbaf8203b9c901721c5d263a05a7a75
JohnSklAdd <ex4786.john@gmail.com>
2019-10-09 14:56:45 +0800

    Delect string.c Add skl_makefile Add string_operations.c

--- src/string_operations.c
+++ src/string_operations.c
@@ -2,6 +2,8 @@
 size_t my_strlen(const char *s)
 {
 	char *p = s;
+	char *p2 = s2;
+	char *p3 = s3;
 	while (*p) ++p;
 	return (p - s);
 }
@@ -15,13 +17,12 @@ char *my_strcpy(const char *t, const char *s)
 
 int main(void) {
 	int i;
-        char p1[32];
+
 	char *s[] = {
 		"Git tutorials", "Hello World"
 	};
 	for (i = 0; i < 2; ++i)
 		printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
-	printf("%s\n", my_strcpy(p1, "Hello, World !!!"));
-    
+	
 	return 0;
 }
